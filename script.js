14c0bfe5b43892743d6f9a9b475e536287e35a3d
Brandon Ketterman <brandonkett24@gmail.com>
2020-06-17 21:31:52 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let passwordlength = prompt("enter a lengtth for this");
+console.log(passwordlength);
+
+let useUppercase = prompt("do you want to use uppercase")
+console.log(useUppercase)
+
+if(useUppercase === 'yes'
+    choices = chioces + uppercaseChars;
+)
